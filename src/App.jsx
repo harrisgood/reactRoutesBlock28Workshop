a8720dee1f921b26f6bf0ad44001769cd974cd39
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-04-02 12:10:53 -0400

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
+import { Link, Routes, Route } from "react-router-dom"
+import Blue from './components/Blue.jsx'
+import Red from './components/Red.jsx'
+import Home from './components/Home.jsx'
 
 function App() {
 
   return (
     <div id="container">
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/">Home</Link>
+        <Link to="/red">Red</Link>
+      </div>
       <div id="main-section">
         <Routes>
-          <Route path='/blue' element={<h1>Blue</h1>} />
-          <Route path='/red' element={<h1>Red</h1>} />
+          <Route path='/blue' element={<Blue />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/red' element={<Red />} />
         </Routes>
       </div>
     </div>
