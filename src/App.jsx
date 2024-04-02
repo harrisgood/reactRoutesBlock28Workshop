031a7ce02b1702cc1a717e966232152c3e0c4683
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-04-02 11:41:45 -0400

    added more starter code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
+    </div>
   )
 }
 
